066ce538830574884b83e8b3ddcd0dd34a97b63e
Parzival7566 <94829251+Parzival7566@users.noreply.github.com>
2022-07-26 11:29:19 +0530

    Add files via upload

---
+++ Driver.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Driver : MonoBehaviour
+{
+    [SerializeField]float steerSpeed = 1f;
+    [SerializeField]float moveSpeed = 0.01f;
+    [SerializeField]float slowSpeed = 10f;
+    [SerializeField]float boostSpeed = 20f;
+    [SerializeField] float destroyDelay = 0.5f;
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        float steerAmount = Input.GetAxis("Horizontal") * steerSpeed * Time.deltaTime;
+        float moveAmount = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
+        transform.Rotate(0,0,-steerAmount);
+        transform.Translate(0,moveAmount,0);
+    }
+    
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Speedup" && moveSpeed!=boostSpeed)
+        {
+            moveSpeed=boostSpeed;
+            Destroy(other.gameObject,destroyDelay);
+        }
+    } 
+    void OnCollisionEnter2D(Collision2D other) 
+   {
+       moveSpeed=slowSpeed;
+   }
+
+}
